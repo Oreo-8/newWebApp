23e5fe36ff4566771c5dfd9f4fa8d60c8475d7c8
WugGan <2546663958@qq.com>
2021-05-06 18:00:56 +0800

    修改主页

--- src/page/blogMain/index.vue
+++ src/page/blogMain/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>blogMain</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="19">
+        <el-card class="box-card">
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="用户管理" name="first" />
+            <el-tab-pane label="配置管理" name="second" />
+            <el-tab-pane label="角色管理" name="third" />
+            <el-tab-pane label="定时任务补偿" name="fourth" />
+          </el-tabs>
+        </el-card>
+      </el-col>
+      <el-col :span="5"><el-card class="box-card"></el-card></el-col>
+    </el-row>
+  </div>
 </template>
 <script>
 export default {
-    name:"blogMain"
+  name: "blogMain",
+  data() {
+    return {
+      activeName: "",
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+  },
 };
 </script>
 <style scoped>
